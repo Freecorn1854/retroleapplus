d3fe045eb7f2de696dc5f2fa8c28e88bc2b264ff
mac2612 <mac2612@foo.com>
2023-08-13 21:17:54 -0600

    Add a basic shutdown monitor to watch poweroff status

---
+++ board/leapfrog/lfx000/fsoverlay/usr/bin/shutdown_monitor.sh
@@ -0,0 +1,13 @@
+# LF1000 doesn't have this file and uses kernel-side power management.
+if [[ -f /sys/devices/platform/lf2000-power/shutdown ]]; then
+    while `true`
+    do
+      if [ "`cat /sys/devices/platform/lf2000-power/shutdown`" -eq "1" ]; then
+        logger -s "Starting user-requested shutdown!"
+        sync
+        poweroff
+        # If we trigger poweroff, we're done.
+        exit
+      fi
+    done
+fi
